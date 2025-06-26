f002ab8f3f0bbcb1fdb1b7f1f175da99c93ddcce
Abhijeet Jha <jhaabhijeet6201@gmail.com>
2025-06-26 23:31:00 +0530

    Adding comments related to object creation

--- src/Circle.java
+++ src/Circle.java
@@ -11,13 +11,14 @@ public class Circle implements Prototype{
     public Circle(Circle circle)
     {
         // setting values of existing circle object inside the new Circle object created through copy constructor
+        // as this.radius means radius of Circle object created through this copy constructor
         this.radius=circle.radius;
         this.colour= circle.colour;
     }
 
     @Override
     public Prototype clone() {
-        // pass values of existing object (passed as this) into new object
+        // pass values of existing Circle object created through main constructor (passed as this) into new object
         return new Circle(this);
     }
 
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         Circle circle=new Circle(10,"Green");
+        //need to cast because clone() returns Prototype
+        //Java won’t assume the result is a Circle unless you explicitly cast
         Circle newcircle=(Circle) circle.clone();
         System.out.println(circle);
         System.out.println(newcircle);
